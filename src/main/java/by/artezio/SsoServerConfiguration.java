beb7f3ac44d087e63abc2773608e0970a61aa14f
Maxim Kotov <mkotov@artezio.com>
2019-11-11 16:47:07 +0300

    create mvc frame

---
+++ src/main/java/by/artezio/SsoServerConfiguration.java
@@ -0,0 +1,23 @@
+package by.artezio;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("by.artezio")
+public class SsoServerConfiguration {
+    @Bean
+    InternalResourceViewResolver internalResourceViewResolver(){
+        InternalResourceViewResolver resolver =
+                 new InternalResourceViewResolver();
+
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/java/by/artezio/SsoServerInitializer.java
@@ -0,0 +1,28 @@
+package by.artezio;
+
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.Set;
+
+public class SsoServerInitializer implements ServletContainerInitializer {
+    @Override
+    public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context =
+                new AnnotationConfigWebApplicationContext();
+
+        context.register(SsoServerConfiguration.class);
+
+        DispatcherServlet servlet = new DispatcherServlet(context);
+
+        ServletRegistration.Dynamic registration =
+                servletContext.addServlet("dispatcher", servlet);
+
+        registration.addMapping("/");
+        registration.setLoadOnStartup(1);
+    }
+}
