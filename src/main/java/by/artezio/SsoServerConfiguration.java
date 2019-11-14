82eacd201fb16f4d2b250f53c1034b68a4126fe9
Maxim Kotov <mkotov@artezio.com>
2019-11-14 17:05:16 +0300

    add liquibase supporting

--- src/main/java/by/artezio/SsoServerConfiguration.java
+++ src/main/java/by/artezio/SsoServerConfiguration.java
@@ -4,12 +4,14 @@ package by.artezio;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("by.artezio")
+@PropertySource("liquibase/liquibase.properties")
 public class SsoServerConfiguration {
     @Bean
     InternalResourceViewResolver internalResourceViewResolver(){
